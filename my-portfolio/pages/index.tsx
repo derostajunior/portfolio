b4c85f84e6a64e6c70976e2ed8175d930f638978
Junior Daquin <juniordeugoue@soft-sec.com>
2024-03-07 12:57:26 +0100

    feat: Add heroIcons and images

--- my-portfolio/pages/index.tsx
+++ my-portfolio/pages/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return <div className="text-9xl text-red-900">HomePage</div>;
 };
 
 export default HomePage;
