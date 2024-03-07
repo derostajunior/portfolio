8e4c7f34f2ff20f2ac1d3da3fdeb01f95b0da910
Junior Daquin <juniordeugoue@soft-sec.com>
2024-03-07 14:58:43 +0100

    feat: implemented  mobile nav bar

--- my-portfolio/pages/index.tsx
+++ my-portfolio/pages/index.tsx
@@ -1,11 +1,18 @@
-import Nav from "@/components/nav";
-import React from "react";
+import React, { useState } from "react";
+import { MobileNav, Nav } from "@/components";
 
 const HomePage = () => {
+  const [nav, setNav] = useState(false);
+  const openNav = () => setNav(true);
+  const closeNav = () => setNav(false);
+
   return (
     <div className="overflow-x-hidden">
       <div>
-        <Nav />
+        //#region Nav Bar
+          <MobileNav nav={nav} closeNav={closeNav} />
+          <Nav openNav={openNav} />
+        //#endregion
       </div>
     </div>
   );
