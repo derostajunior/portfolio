4fa3ec2ee45f20e06bd0899319030d7793a83dcc
Junior Daquin <juniordeugoue@soft-sec.com>
2024-03-07 19:08:38 +0100

    feat: Implemented Text effect

--- my-portfolio/components/index.ts
+++ my-portfolio/components/index.ts
@@ -2,10 +2,12 @@ import Hero from "./hero";
 import MobileNav from "./mobile-nav";
 import Nav from "./nav";
 import Particle from "./Particle";
+import TextEffect from "./text-effect"
 
 export {
     Hero,
     MobileNav,
     Nav,
-    Particle
+    Particle,
+    TextEffect
 }
