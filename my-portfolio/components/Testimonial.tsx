568e13cebe453d0b46bf15820ad43c6a90b4ffed
Junior Daquin <juniordeugoue@soft-sec.com>
2024-04-11 15:19:00 +0100

    feat: Add the data for testimonial

--- my-portfolio/components/Testimonial.tsx
+++ my-portfolio/components/Testimonial.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import TestimonialSlider from "./testimonial-slider";
+import { testimonialData } from "@/assets/lib/data";
 
 const Testimonial = () => {
     return (
@@ -8,7 +9,7 @@ const Testimonial = () => {
                 Client <span className="text-[#b03320]">Review</span>
             </h1>
             <div className="pt-[5rem] pb-[4rem] w-[80%] mx-auto">
-                <TestimonialSlider />
+                <TestimonialSlider TestimonialData={testimonialData}/>
             </div>
         </div>
     )
